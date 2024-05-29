c67d995ebdd1aded082af72b5b89282d20aed586
Divye2401 <52701687+Divye2401@users.noreply.github.com>
2024-05-28 20:53:44 -0400

    Made changes to code

--- client/client.py
+++ client/client.py
@@ -1,5 +1,5 @@
 import socket
-
+import os
 
 class Client:
     def __init__(self, host, port):
@@ -18,25 +18,49 @@ class Client:
         :param eof_token: a token that denotes the end of the message.
         :return: a bytearray message with the eof_token stripped from the end.
         """
-        raise NotImplementedError('Your implementation here.')
+        
+        msg = bytearray()
+        while True:
+                packet = active_socket.recv(buffer_size)
+                
+                if packet[-10:] == eof_token.encode():
+                    msg += packet[:-10]
+                    break
+                msg += packet
+                
+
+        return msg
 
     def initialize(self, host, port):
-        """
-        1) Creates a socket object and connects to the server.
-        2) receives the random token (10 bytes) used to indicate end of messages.
-        3) Displays the current working directory returned from the server (output of get_working_directory_info() at the server).
-        Use the helper method: receive_message_ending_with_token() to receive the message from the server.
-        :param host: the ip address of the server
-        :param port: the port number of the server
-        :return: the created socket object
-        :return: the eof_token
-        """
+                """
+                1) Creates a socket object and connects to the server.
+                2) receives the random token (10 bytes) used to indicate end of messages.
+                3) Displays the current working directory returned from the server (output of get_working_directory_info() at the server).
+                Use the helper method: receive_message_ending_with_token() to receive the message from the server.
+                :param host: the ip address of the server
+                :param port: the port number of the server
+                :return: the created socket object
+                :return: the eof_token
+                """
 
         # print('Connected to server at IP:', host, 'and Port:', port)
 
         # print('Handshake Done. EOF is:', eof_token)
 
-        raise NotImplementedError('Your implementation here.')
+                self.client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.client_socket.connect((host, port))
+                print('Connected to server at IP:', host, 'and Port:', port)
+
+                self.eof_token=self.client_socket.recv(1024).decode()
+                print('Handshake Done. EOF is:', self.eof_token)
+
+
+                cwd=self.receive_message_ending_with_token(self.client_socket,1024,self.eof_token)
+                print(f'current working directory of server : {cwd.decode()}')
+
+
+
+                return self.client_socket,self.eof_token
 
 
 
@@ -49,7 +73,15 @@ class Client:
         :param client_socket: the active client socket object.
         :param eof_token: a token to indicate the end of the message.
         """
-        raise NotImplementedError('Your implementation here.')
+        command_and_arg+=eof_token
+    
+        self.client_socket.sendall(command_and_arg.encode())
+
+        new_dir=self.receive_message_ending_with_token(client_socket,1024,eof_token)
+        print(f' New working directory changed to : {new_dir.decode()}')
+
+        
+
 
     def issue_mkdir(self, command_and_arg, client_socket, eof_token):
         """
@@ -60,7 +92,13 @@ class Client:
         :param client_socket: the active client socket object.
         :param eof_token: a token to indicate the end of the message.
         """
-        raise NotImplementedError('Your implementation here.')
+        command_and_arg+=eof_token
+        client_socket.sendall(command_and_arg.encode())
+
+        new_dir=self.receive_message_ending_with_token(client_socket,1024,eof_token)
+        print(f' {new_dir.decode()}')
+     
+
 
     def issue_rm(self, command_and_arg, client_socket, eof_token):
         """
@@ -71,7 +109,12 @@ class Client:
         :param client_socket: the active client socket object.
         :param eof_token: a token to indicate the end of the message.
         """
-        raise NotImplementedError('Your implementation here.')
+        command_and_arg+=eof_token
+        client_socket.sendall(command_and_arg.encode())
+
+        remove_dir=self.receive_message_ending_with_token(client_socket,1024,eof_token)
+        print(f' {remove_dir.decode()}')
+  
 
     def issue_ul(self, command_and_arg, client_socket, eof_token):
         """
@@ -82,7 +125,22 @@ class Client:
         :param client_socket: the active client socket object.
         :param eof_token: a token to indicate the end of the message.
         """
-        raise NotImplementedError('Your implementation here.')
+        file_name=command_and_arg.split()[1]
+        command_and_arg+=eof_token
+        client_socket.sendall(command_and_arg.encode()) #Sending command
+
+        current_working_directory=os.getcwd()
+        file_path=current_working_directory+'/'+file_name
+        with open(file_path, 'rb') as f:
+                file_content = f.read()
+
+        file_content+=eof_token.encode()
+        client_socket.sendall(file_content) #Sending file
+
+        ack_msg=self.receive_message_ending_with_token(client_socket,1024,eof_token) #Recieved once file succesffully uploaded
+        print(f' {ack_msg.decode()}')
+
+
 
     def issue_dl(self, command_and_arg, client_socket, eof_token):
         """
@@ -95,7 +153,20 @@ class Client:
         :param eof_token: a token to indicate the end of the message.
         :return:
         """
-        raise NotImplementedError('Your implementation here.')
+        file_name=command_and_arg.split()[1]
+        command_and_arg+=eof_token
+        client_socket.sendall(command_and_arg.encode())
+
+        file_content=self.receive_message_ending_with_token(client_socket,1024,eof_token)
+        
+        file_path=os.getcwd()+'/'+file_name
+        with open(file_path, 'wb') as f:
+            f.write(file_content)
+        print(f'Downloaded file {file_name}')    
+
+
+
+ 
 
     def issue_info(self,command_and_arg, client_socket, eof_token):
         """
@@ -107,7 +178,12 @@ class Client:
         :param eof_token: a token to indicate the end of the message.
         :return: the size of file in string
         """
-        raise NotImplementedError('Your implementation here.')
+        command_and_arg+=eof_token
+        client_socket.sendall(command_and_arg.encode())
+
+        msg=self.receive_message_ending_with_token(client_socket,1024,eof_token)
+        print(f' {msg.decode()}')
+
 
 
     def issue_mv(self,command_and_arg, client_socket, eof_token):
@@ -119,22 +195,61 @@ class Client:
         :param client_socket: the active client socket object.
         :param eof_token: a token to indicate the end of the message.
         """
-        raise NotImplementedError('Your implementation here.')
+        command_and_arg+=eof_token
+        client_socket.sendall(command_and_arg.encode())
+
+        msg=self.receive_message_ending_with_token(client_socket,1024,eof_token)
+        print(f' {msg.decode()}')
+
+        
+
+    
 
     def start(self):
         """
         1) Initialization
         2) Accepts user input and issue commands until exit.
         """
-        # initialize
+        client_socket,eof_token=self.initialize(self.host,self.port)
+       
+        while True:
+            command = input('Enter your input command: ')
+            str=command.split()[0]
+            match str:
+                case "cd":
+                    self.issue_cd(command,client_socket,eof_token)
 
-        raise NotImplementedError('Your implementation here.')
-        # while True:
-            # get user input
+                case "mkdir":
+                    self.issue_mkdir(command,client_socket,eof_token)
 
-            # call the corresponding command function or exit
+                case "rm":
+                    self.issue_rm(command,client_socket,eof_token)
+                
+                case "mv":
+                    self.issue_mv(command,client_socket,eof_token)
 
-        # print('Exiting the application.')
+                case "dl":
+                    self.issue_dl(command,client_socket,eof_token)
+
+                case "ul":
+                    self.issue_ul(command,client_socket,eof_token)
+
+                case "info":
+                    self.issue_info(command,client_socket,eof_token)
+
+                case "exit":
+                    
+                    client_socket.sendall((command+eof_token).encode())
+                    print("Exiting the system")
+                    break
+                case _:
+                    print("Enter a valid command")
+
+            cwd=self.receive_message_ending_with_token(client_socket,1024,eof_token) ## Display current directory info
+            print(f'current working directory of server : {cwd.decode()}')
+
+        print('Exiting the application.')
+        self.client_socket.close()
 
 
 def run_client():
--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ import os
 import shutil
 from pathlib import Path
 import time
+import string
 
 class Server:
     def __init__(self, host, port):
@@ -13,12 +14,12 @@ class Server:
         self.server_socket = None
 
     def start(self):
-        """
-        1) Create server, bind and start listening.
-        2) Accept clinet connections and serve the requested commands.
+                """
+                1) Create server, bind and start listening.
+                2) Accept clinet connections and serve the requested commands.
 
-        Note: Use ClientThread for each client connection.
-        """
+                Note: Use ClientThread for each client connection.
+                """
             # Create a socket
 
             # Bind the socket to the specified address and port
@@ -40,7 +41,34 @@ class Server:
             #     print("Connection closed.")
             #     client_socket.close()
 
-        raise NotImplementedError("Your implementation here.")
+
+
+                self.server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+                self.server_socket.bind((self.host, self.port))
+                self.server_socket.listen()
+
+                print(f"Server listening on {self.host}:{self.port}")
+                
+                while True:
+                    client_socket, client_address = self.server_socket.accept()
+                    print(f"Accepted connection from {client_address}")
+                    eof_token = self.generate_random_eof_token()
+                    client_socket.sendall(eof_token.encode())
+                    
+                    try:
+                        
+                        client_thread = ClientThread(self,client_socket,client_address,eof_token)
+                        client_thread.start()
+                        client_thread.join()
+                    except Exception as e:
+                        print(f"Error: {e}")
+                    finally:
+                        print("Connection closed.")
+                        client_socket.close()
+
+           
+
+        
 
 
 
@@ -57,6 +85,9 @@ class Server:
         files = "\n-- " + "\n-- ".join(
             [i.name for i in Path(working_directory).iterdir() if i.is_file()]
         )
+
+     #   print(dirs)
+     #   print(files)
         dir_info = f"Current Directory: {working_directory}:\n|{dirs}{files}"
         return dir_info
 
@@ -66,7 +97,11 @@ class Server:
         Examples: '<1f56xc5d>', '<KfOVnVMV>'
         return: the generated token.
         """
-        raise NotImplementedError("Your implementation here.")
+        random_str = ''.join(random.choices(string.ascii_letters + string.digits, k = 8))
+        token='<'+str(random_str)+'>'
+        
+        return token
+        
 
     def receive_message_ending_with_token(self, active_socket, buffer_size, eof_token):
         """
@@ -78,96 +113,135 @@ class Server:
         :param eof_token: a token that denotes the end of the message.
         :return: a bytearray message with the eof_token stripped from the end.
         """
-        raise NotImplementedError("Your implementation here.")
+        msg = bytearray()
+        while True:
+                packet = active_socket.recv(buffer_size)
+                if packet[-10:] == eof_token.encode():
+                    msg += packet[:-10]
+                    break
+                msg += packet
+
+        return msg
+
+
+
 
     def handle_cd(self, current_working_directory, new_working_directory):
-        """
-        Handles the client cd commands. Reads the client command and changes the current_working_directory variable
-        accordingly. Returns the absolute path of the new current working directory.
-        :param current_working_directory: string of current working directory
-        :param new_working_directory: name of the sub directory or '..' for parent
-        :return: absolute path of new current working directory
-        """
-        raise NotImplementedError("Your implementation here.")
+            """
+            Handles the client cd commands. Reads the client command and changes the current_working_directory variable
+            accordingly. Returns the absolute path of the new current working directory.
+            :param current_working_directory: string of current working directory
+            :param new_working_directory: name of the sub directory or '..' for parent
+            :return: absolute path of new current working directory
+            """
+            os.chdir(new_working_directory)
+            return os.getcwd()
 
     def handle_mkdir(self, current_working_directory, directory_name):
-        """
-        Handles the client mkdir commands. Creates a new sub directory with the given name in the current working directory.
-        :param current_working_directory: string of current working directory
-        :param directory_name: name of new sub directory
-        """
-        raise NotImplementedError("Your implementation here.")
+            """
+            Handles the client mkdir commands. Creates a new sub directory with the given name in the current working directory.
+            :param current_working_directory: string of current working directory
+            :param directory_name: name of new sub directory
+            """
+            pathto_newdir=current_working_directory+'/'+directory_name
+            os.makedirs(pathto_newdir)
 
     def handle_rm(self, current_working_directory, object_name):
-        """
-        Handles the client rm commands. Removes the given file or sub directory. Uses the appropriate removal method
-        based on the object type (directory/file).
-        :param current_working_directory: string of current working directory
-        :param object_name: name of sub directory or file to remove
-        """
-        raise NotImplementedError("Your implementation here.")
+            """
+            Handles the client rm commands. Removes the given file or sub directory. Uses the appropriate removal method
+            based on the object type (directory/file).
+            :param current_working_directory: string of current working directory
+            :param object_name: name of sub directory or file to remove
+            """
+            removedir=current_working_directory+'/'+object_name
+            if object_name.find(".") == -1: # IF object is a directory
+                shutil.rmtree(removedir)
+            else:
+                os.remove(removedir)
 
     def handle_ul(
         self, current_working_directory, file_name, service_socket, eof_token
     ):
-        """
-        Handles the client ul commands. First, it reads the payload, i.e. file content from the client, then creates the
-        file in the current working directory.
-        Use the helper method: receive_message_ending_with_token() to receive the message from the client.
-        :param current_working_directory: string of current working directory
-        :param file_name: name of the file to be created.
-        :param service_socket: active socket with the client to read the payload/contents from.
-        :param eof_token: a token to indicate the end of the message.
-        """
-        raise NotImplementedError("Your implementation here.")
+            """
+            Handles the client ul commands. First, it reads the payload, i.e. file content from the client, then creates the
+            file in the current working directory.
+            Use the helper method: receive_message_ending_with_token() to receive the message from the client.
+            :param current_working_directory: string of current working directory
+            :param file_name: name of the file to be created.
+            :param service_socket: active socket with the client to read the payload/contents from.
+            :param eof_token: a token to indicate the end of the message.
+            """
+            file_content=self.receive_message_ending_with_token(service_socket,1024,eof_token)
+            file_path=os.getcwd()+'/'+file_name
+            with open(file_path, 'wb') as f:
+                    f.write(file_content)
+            
+           
 
     def handle_dl(
         self, current_working_directory, file_name, service_socket, eof_token
     ):
-        """
-        Handles the client dl commands. First, it loads the given file as binary, then sends it to the client via the
-        given socket.
-        :param current_working_directory: string of current working directory
-        :param file_name: name of the file to be sent to client
-        :param service_socket: active service socket with the client
-        :param eof_token: a token to indicate the end of the message.
-        """
-        raise NotImplementedError("Your implementation here.")
+            """
+            Handles the client dl commands. First, it loads the given file as binary, then sends it to the client via the
+            given socket.
+            :param current_working_directory: string of current working directory
+            :param file_name: name of the file to be sent to client
+            :param service_socket: active service socket with the client
+            :param eof_token: a token to indicate the end of the message.
+            """
+            file_path=current_working_directory+'/'+file_name
+            with open(file_path, 'rb') as f:
+                file_content = f.read()
 
-    def handle_info(self,current_working_directory, file_name):
-        """
-        Handles the client info commands. Reads the size of a given file. 
-        :param current_working_directory: string of current working directory
-        :param file_name: name of sub directory or file to remove
-        """
-        raise NotImplementedError('Your implementation here.')
+            file_content+=eof_token.encode()
+            service_socket.sendall(file_content)
+
+
+    def handle_info(
+        self, current_working_directory, file_name, service_socket, eof_token
+    ):
+            """
+            Handles the client info commands. Reads the size of a given file. 
+            :param current_working_directory: string of current working directory
+            :param file_name: name of sub directory or file to remove
+            :param service_socket: active service socket with the client
+            :param eof_token: a token to indicate the end of the message.
+            """
+            file_path=current_working_directory+'/'+file_name
+            with open(file_path, 'rb') as f:
+                file_content = f.read()
+
+            file_size=str(len(file_content))
+            msg='The size of file '+file_name+' is '+file_size+' bytes'+eof_token
+            service_socket.sendall(msg.encode())
     
     def handle_mv(self,current_working_directory, file_name, destination_name):
-        """
-        Handles the client mv commands. First, it looks for the file in the current directory, then it moves or renames 
-        to the destination file depending on the nature of the request.
-        :param current_working_directory: string of current working directory
-        :param file_name: name of the file tp be moved / renamed
-        :param destination_name: destination directory or new filename
-        """
-        raise NotImplementedError('Your implementation here.')
+            """
+            Handles the client mv commands. First, it looks for the file in the current directory, then it moves or renames 
+            to the destination file depending on the nature of the request.
+            :param current_working_directory: string of current working directory
+            :param file_name: name of the file tp be moved / renamed
+            :param destination_name: destination directory or new filename
+            """
+            orig_dir=current_working_directory+'/'+file_name
+            new_dir=destination_name+'/'+file_name
+            shutil.move(orig_dir,new_dir)
+
+    
 
 
 class ClientThread(Thread):
-    def __init__(self, server: Server, service_socket: socket.socket, address: str):
+    def __init__(self, server: Server, service_socket: socket.socket, address: str, eof_token: str):
         Thread.__init__(self)
         self.server_obj = server
         self.service_socket = service_socket
         self.address = address
+        self.eof_token = eof_token
 
     def run(self):
         # print ("Connection from : ", self.address)
-        raise NotImplementedError("Your implementation here.")
-
         # establish working directory
-
         # send the current dir info
-
         # while True:
         # get the command and arguments and call the corresponding method
 
@@ -176,6 +250,83 @@ class ClientThread(Thread):
         # send current dir info
 
         # print('Connection closed from:', self.address)
+
+
+        print ("Connection from : ", self.address)
+        cwd=os.getcwd()
+        cwd_info=self.server_obj.get_working_directory_info(cwd)
+        cwd_info+=self.eof_token
+        self.service_socket.sendall(cwd_info.encode())
+        
+        while True:
+                
+                command=self.server_obj.receive_message_ending_with_token(self.service_socket,1024,self.eof_token).decode()
+                str=command.split()[0]
+                match str:
+                    case "cd":
+                        current_working_directory=os.getcwd()
+                        new_working_directory=command.split()[1]
+                        updated_dir=self.server_obj.handle_cd(current_working_directory,new_working_directory)
+                       
+                        updated_dir+=self.eof_token
+                        self.service_socket.sendall(updated_dir.encode())
+
+                    case "mkdir":
+                        current_working_directory=os.getcwd()
+                        directory_name=command.split()[1]
+                        self.server_obj.handle_mkdir(current_working_directory,directory_name)
+
+                        msg='Created new directory at '+current_working_directory+'/'+directory_name+self.eof_token
+                        self.service_socket.sendall(msg.encode())
+
+                    case "rm":
+                        current_working_directory=os.getcwd()
+                        object_name=command.split()[1]
+                        self.server_obj.handle_rm(current_working_directory,object_name)
+
+                        msg='Removed object at '+current_working_directory+'/'+object_name+self.eof_token
+                        self.service_socket.sendall(msg.encode())
+                    
+                    case "mv":
+                        current_working_directory=os.getcwd()
+                        file_name=command.split()[1]
+                        destination_name=command.split()[2]
+                        self.server_obj.handle_mv(current_working_directory,file_name,destination_name)
+
+                        msg='Moved object at '+current_working_directory+'/'+file_name+'to location at'+destination_name+'/'+file_name+self.eof_token
+                        self.service_socket.sendall(msg.encode())
+
+                    case "dl":
+                        current_working_directory=os.getcwd()
+                        file_name=command.split()[1]
+                        self.server_obj.handle_dl(current_working_directory,file_name,self.service_socket,self.eof_token)
+
+                    case "ul":
+                        current_working_directory=os.getcwd()
+                        file_name=command.split()[1]
+                        self.server_obj.handle_ul(current_working_directory,file_name,self.service_socket,self.eof_token)
+
+                        msg='Uploaded file at '+current_working_directory+'/'+file_name+self.eof_token
+                        self.service_socket.sendall(msg.encode())
+
+                    case "info":
+                        current_working_directory=os.getcwd()
+                        file_name=command.split()[1]
+                        self.server_obj.handle_info(current_working_directory,file_name,self.service_socket,self.eof_token)
+
+                    case "exit":
+                        
+                        break
+                    
+                time.sleep(1)
+
+           #     cwd=os.getcwd()              #Comment these 4 lines to get the proper output from TEST.py
+            #    cwd_info=self.server_obj.get_working_directory_info(cwd)       # Because the test.py directly calls the functions 
+            #    cwd_info+=self.eof_token                                       # instead of using the prompts from user
+             #   self.service_socket.sendall(cwd_info.encode())                 # this messes with the output sent to the client
+
+        print('Connection closed from:', self.address)
+       
 
 
 def run_server():
